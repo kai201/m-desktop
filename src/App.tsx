f5c264efdfad4de620e6b072b3f1d7ebfefe607a
kai201 <kai201@163.com>
2025-03-25 19:56:45 +0800

    fix.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 import { useEffect } from "react";
 import { listen } from "@tauri-apps/api/event";
 import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
-
+import { check } from "@tauri-apps/plugin-updater";
 function App() {
   useEffect(() => {
     let unlisten: () => void;
@@ -16,7 +16,7 @@ function App() {
         let data = e.payload as any;
       });
     })();
-    
+
     initOnApp();
 
     return () => unlisten?.();
@@ -40,6 +40,37 @@ function App() {
   async function hanlderWins() {
     invoke("get_win_all");
   }
+  async function hanlderUpdate() {
+    const update = await check();
+    if (update) {
+      console.log(
+        `found update ${update.version} from ${update.date} with notes ${update.body}`
+      );
+      let downloaded = 0;
+      let contentLength = 0;
+      // alternatively we could also call update.download() and update.install() separately
+      await update.downloadAndInstall((event) => {
+        switch (event.event) {
+          case "Started":
+            contentLength = event.data.contentLength || 0;
+            console.log(
+              `started downloading ${event.data.contentLength} bytes`
+            );
+            break;
+          case "Progress":
+            downloaded += event.data.chunkLength;
+            console.log(`downloaded ${downloaded} from ${contentLength}`);
+            break;
+          case "Finished":
+            console.log("download finished");
+            break;
+        }
+      });
+
+      console.log("update installed");
+      // await relaunch();
+    } 
+  }
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
@@ -62,6 +93,7 @@ function App() {
       <button onClick={hanlderWins}>windows</button>
       <button onClick={hanlderStart}>start</button>
       <button onClick={hanlderStop}>stop</button>
+      <button onClick={hanlderUpdate}>update</button>
     </main>
   );
 }
