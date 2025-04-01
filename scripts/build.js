52c4e2a1f4962a36a5c29ca8d0dc16caca05630f
kai201 <kai201@163.com>
2025-04-01 18:17:00 +0800

    fix.

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,49 @@
-const env = process.env.NODE_ENV || "development";
+import path from "node:path";
+import fs from "node:fs";
+import { mergeConfig } from "vite";
 
-console.log(env);
+let configName = path.join("src-tauri/tauri.conf.json");
+let configStr = fs.readFileSync(configName, "utf8");
+
+const config = JSON.parse(configStr);
+const env = process.env.NODE_ENV || "development";
+const version = process.env.VERSION || "0.1.0";
+
+let opts = {};
+
+opts = {
+  version: version,
+  bundle: {
+    windows: {
+      webviewInstallMode: {
+        type: "embedBootstrapper",
+      },
+      nsis: {
+        minimumWebview2Version: "110.0.1531.0",
+      },
+    },
+  },
+  plugins: {},
+};
+
+if (env == "pro") {
+  config.plugins.updater.endpoints = null;
+  opts.plugins = {
+    updater: {
+      endpoints: [
+        "https://whelp.ares-ai.cn/sys/version/{{target}}/{{arch}}/{{current_version}}",
+      ],
+    },
+  };
+}
+
+opts = mergeConfig(config, opts);
+
+const stable = path.join("src-tauri/tauri.conf.stable.json");
+const standard = path.join("src-tauri/tauri.conf.standard.json");
+fs.writeFileSync(standard, JSON.stringify(opts, null, "\t"));
+opts.bundle.windows.webviewInstallMode = {
+  type: "fixedRuntime",
+  path: "./Microsoft.WebView2.FixedVersionRuntime.134.0.3124.93.x64/",
+};
+fs.writeFileSync(stable, JSON.stringify(opts, null, "\t"));
